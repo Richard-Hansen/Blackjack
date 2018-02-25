6cb96c7095078dc141e9a510ef191340b698a127
Richard Hansen <richie.hansen@gmail.com>
2018-02-24 22:07:37 -0500

    Completed buildDeck

--- Blackjack.py
+++ Blackjack.py
@@ -1,4 +1,5 @@
 #Let's play some blackjack!
+import random
 
 class Player(object):
 
@@ -8,6 +9,18 @@ class Player(object):
 	def checkBank(self):
 		return self.__bankroll
 
+class Game(object):
+
+	def __init__(self):
+		self.deck = self.buildDeck()
+
+	def buildDeck(self):
+		return [(10 if i > 10 else i) for i in range(1,14) for j in range (0,4)]
+
+
 if __name__ == "__main__":
 	p = Player()
 	print p.checkBank()
+
+	g = Game()
+	print g.deck
